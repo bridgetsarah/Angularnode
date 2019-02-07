b35520981a364ed47b73774a7929e4394a3677e4
Bridget Sarah <msbskelly@gmail.com>
2019-02-07 10:49:27 +0000

    Added Mongoose, Added DB for Mongo

---
+++ api/db.js
@@ -0,0 +1,11 @@
+// Database File 
+
+//Require Mongoose & Database connection//
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/social', function(){
+    console.log('mongodb connected')
+})
+module.exports = mongoose
+
+
+
