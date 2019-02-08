30646a800e08472813021dae343c86d8caa23716
Bridget Sarah <msbskelly@gmail.com>
2019-02-08 20:31:29 +0000

    Added custom gulp task for css

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -3,8 +3,7 @@ var concat = require('gulp-concat')
 var sourcemaps = require('gulp-sourcemaps')
 var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
-var fs = require('fs')
-
+var stylus = require('gulp-stylus')
 
 gulp.task('js', function(require){
     gulp.src(['ng/module.js', 'ng/**/*.js'])
@@ -16,14 +15,22 @@ gulp.task('js', function(require){
     .pipe(gulp.dest('assets'))
 })
 
+gulp.task('css', function(require){
+    gulp.src(['css/app.styl', 'css/app.styl'])
+    .pipe(uglify())
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/'));
+});
+
+
 //-----------------Gulp watchers -----------------------
-/
+
 //-----------------Gulp JS Watcher----------------------// Watching NG folder//
 gulp.task('watch:js', ['js'], function(){
     gulp.watch('ng/**/*.js', ['js'])
 })
 //-----------------Gulp CSS Watcher---------------------// Watching CSS folder
-gulp.task('watch:css', function(){
+gulp.task('watch:css', function(require){
     gulp.watch('css/**/*.styl', ['css'])
 })
 
--- api/newFile.js
+++
@@ -1,7 +0,0 @@
-    var fs = require('fs');
-    //read files under the gulp folder directory!!
-    fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
-        require('./gulp/' + task);
-    });
-}
-
