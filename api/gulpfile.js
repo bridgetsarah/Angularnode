00011c63578c070e061d48cbf555d1d1e18378a9
Bridget Sarah <msbskelly@gmail.com>
2019-02-08 13:27:09 +0000

    explicitly signalling task for completion... gulp.

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -6,7 +6,8 @@ var ngAnnotate = require('gulp-ng-annotate')
 gulp.task('js', function(){
     gulp.src(['ng/module.js', 'ng/**/*.js'])
     .pipe(concat('app.js'))
-    .pipe(ngAnnotate(require))
-    .pipe(uglify(require))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
     .pipe(gulp.dest('assets'))
-})
+     done();
+});
