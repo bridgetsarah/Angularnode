199587db4bf7db37761d28a20b85878c5ceed2f2
Bridget Sarah <msbskelly@gmail.com>
2019-02-09 14:29:49 +0000

    Added Express Authrentication

---
+++ api/controllers/server.js
@@ -0,0 +1,4 @@
+// Api/Controllers/Server.js
+
+app.use('/api/sessions', require('./controllers/api/sessions'))
+app.use('/api/users', require('./controllers/api/users'))
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // Configuration File for Application
+//
+// - Good Place to store, API Keys, Database configuration
 
 module.exports = {
     secret: 'supersecretkey'
