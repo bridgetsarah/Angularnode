6f80564d63082506b87db2e271680290fa689676
Bridget Sarah <msbskelly@gmail.com>
2019-02-09 14:04:35 +0000

    Added Angular Login Module Controller to NG Folder route

---
+++ api/ng/login.ctrl.js
@@ -0,0 +1,11 @@
+// api/ng/login controller
+
+angular.module('app')
+    .controller('LoginCtrl', function ($scope, UserSvc){
+        $scope.login = function (username, password) {
+            UserSvc.login(username, password)
+            .then(function (user){
+                console.log(user)
+            })
+        }
+    })
