17e1a3e40b0927291a0804a1af80cb3384eae65d
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 15:36:19 +0000

    Added Gup-Data to help CSS

--- api/gulp/css.js
+++ api/gulp/css.js
@@ -1,8 +1,16 @@
-var gulp = require('gulp')
-var stylus = require('gulp-stylus')
+var gulp = require('gulp');
+var data = require('gulp-data');
+var stylus = require('gulp-stylus');
 
-gulp.task('css', function (){
-    gulp.scr('css/**/*.styl')
+//gulp.task('css', function (){
+ //   gulp.scr('css/**/*.styl')
+ //   .pipe(stylus())
+ //   .pipe(gulp.dest('assets'))
+//})
+
+///////// new gulp css task added
+gulp.task('css', function(){
+    return gulp.src('css/**/*/.styl')
     .pipe(stylus())
     .pipe(gulp.dest('assets'))
 })
