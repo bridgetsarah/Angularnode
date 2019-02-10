77705ae89cbb63d94c6415e670e906c68628bc46
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 18:23:56 +0000

    Fixed: Routes js

--- api/ng/routes.js
+++ api/ng/routes.js
@@ -3,7 +3,7 @@
 angular.module('app')
 .config(function ($routeProvider){
     $routeProvider
-    .when('/', { controller: 'Postsctrl', templateUrl: 'posts.html'})
-    .when('/register', {controller: 'registerCtrl', templateUrl: 'register.html'})
-    .when('login', {controller: 'LoginCtrl', templateUrl: 'login.html'})
+    .when('/', { controller: 'Postsctrl', templateUrl: 'posts.html' })
+    .when('/register', {controller: 'registerCtrl', templateUrl: 'register.html' })
+    .when('login', {controller: 'LoginCtrl', templateUrl: 'login.html' })
 })
