89b2fa2e02f4a46474768cb35c01323d7038ef8c
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 18:30:57 +0000

    Added: Register function to ng/user.srvc.js

--- api/ng/user.svc.js
+++ api/ng/user.svc.js
@@ -1,40 +1,28 @@
 // Api/ng/user.svc.js file
-
 angular.module('app')
-.service('UserSvc', function($http){
-    var svc = thissvc.getUser = function (){
+.service('UserSvc', function($http){    ///User Service
+    var svc = this
+    svc.getUser = function (){
         return $http.get('/api/users')
+        .then(function (response){
+            return response.data
+        })
     }
-svc.login = function (username, password){
+svc.login = function (username, password){  // Login services
     return $http.post('/api/sessions', {
         username: username, password: password
     }).then(function (val) {
-        svc.token = val.data
-        $http.defaults.header.common['X-Auth'] = val.data
+        svc.token = response.data
+        $http.defaults.header.common['X-Auth'] = response.data
         return svc.getUser()
     })
   }
 })
 
-
-
-
-//Calling Post to API
-.service('UserSvc'), function ($http) {
-    var svc = this
-    svc.getUser = function(){
-        return $http.get('api/users',{
-            headers: { 'X-Auth' : this.token }
-        })
-    }
-}
-
-//Calling Get to API
-svc.login = function (username, password){
-    return $http.post('api/sessions', {
+svc.register = function (username, password) {
+    return $http.post('/api/users', {
         username: username, password: password
-    }).then(function (val){
-        svc.token = val.data
-        return svc.getUser()
+    }).then(function (){
+        return svc.login(username, password)
     })
 }
