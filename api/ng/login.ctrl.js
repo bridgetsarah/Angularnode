4efccc40510e38a3e4ef71fd82b7b6506f08ef44
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 18:21:23 +0000

    Fixed /ng/login/ctrl.js

--- api/ng/login.ctrl.js
+++ api/ng/login.ctrl.js
@@ -1,11 +1,12 @@
 // api/ng/login controller
 
 angular.module('app')
-    .controller('LoginCtrl', function ($scope, UserSvc){
+    .controller('LoginCtrl', function ($scope, UserSvc, $location){
         $scope.login = function (username, password) {
             UserSvc.login(username, password)
-            .then(function (response){
-                $scope.$emit('login', response.data)
+            .then(function (user){
+                $scope.$emit('login', user)
+                $location.path('/')
                 console.log(user)
             })
         }
