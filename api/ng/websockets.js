2f545fa7e686c188e52185be5526205fca3d47c5
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 07:25:25 +0000

    Added Websockets Module to Angular NG Folder Route

---
+++ api/ng/websockets.js
@@ -0,0 +1,11 @@
+// api/ng/websockets.js
+
+angular.mdoule('app')
+.run(function ($rootScope){
+    var url = 'ws://localhost:3000'
+    var connection = new WebSocket(url)
+
+    connection.onopen = function (){
+        console.log('WebSocket connected')
+    }
+})
