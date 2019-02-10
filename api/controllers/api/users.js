c2a05d8b4a0527b0063b575ce542cf8977449b4c
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 16:26:43 +0000

    Fixed: Typo : Controllers/api/users.js

--- api/controllers/api/users.js
+++ api/controllers/api/users.js
@@ -1,21 +1,20 @@
 // api/controllers/api/users.js
 
 var router = require('express').Router
-var bcypt = require('bcrypt')
+var bcrypt = require('bcrypt')
 var jwt = require('jwt-simple')
 var User = require('../../models/user')
 var config = require('../../config')
 
 router.get('/', function (req, res, next){
     if (!req.headers['x-auth']){
-        return res.send(401)
+        return res.sendStatus(401)
     }
-})
-
-var auth = jwt.decode(req.headers['x-auth'], config.secret)
-User.findOne({username: auth.username}, function (err, user){
-    if (err) { return next (err) }
-    res.json(user)
+    var auth = jwt.decode(req.headers['x-auth'], config.secret)
+    User.findOne({username: auth.username}, function (err, user){
+        if (err) { return next (err) }
+        res.json(user)
+    })
 })
 
 router.post('/', function (req, res, next){
@@ -25,7 +24,7 @@ router.post('/', function (req, res, next){
         user.password = hash
         user.save(function (err) {
             if (err) { return next(err) }
-                res.send(201)
+                res.sendStatus(201)
         })
     })
 })
