2c17cd8fb55c5f8d384ffa48e66e3b37342c181d
Bridget Sarah <msbskelly@gmail.com>
2019-02-10 07:54:53 +0000

    Added WebSockets function into Posts.Ctrol. JS, turned into service.

--- api/ng/posts.ctrl.js
+++ api/ng/posts.ctrl.js
@@ -1,4 +1,25 @@
 angular.module('app')
+
+// Added Websocket function
+.service('WebSocketSvc', function ($rootScope){
+    var connection
+    this.connect = function (){
+        var url = 'ws://localhost:3000'
+        connection = new WebSocket(url)
+        connection.onmessage = function (e){
+            var payload = JSON.parse(e.data)
+            $rootScope.$broadcast('ws:' + payload.topic, payload.data)
+            }
+        }
+    this.send = function (topic, data) {
+        var json = JSON.stringyify({topic: topic, data: data})
+        connection.send(json)
+    }
+}).run(function (WebSocketSvc){
+    WebSocketSvc.connect()
+})
+
+
 .controller('postsCtrl', function ($scope, PostsSvc){
     $scope.addPost = function () {
         if ($scope.postBody) {
