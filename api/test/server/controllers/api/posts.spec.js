9fccd9da60d7c9d57edd089c61f2f23ab6e1bc62
Bridget Sarah <msbskelly@gmail.com>
2019-02-11 14:17:02 +0000

    Added: Test/server/controllers/api/post.spec.js

---
+++ api/test/server/controllers/api/posts.spec.js
@@ -0,0 +1,10 @@
+// Node Testing //Test/server/controllers/api/post.spec.js
+
+var expect = require('chai')
+crtl = require('../../../../controllers/api/posts')
+
+describe('controllers.api.posts', function() {
+    it('exists', function(){
+        expect(ctrl).to.exist
+    })
+})
