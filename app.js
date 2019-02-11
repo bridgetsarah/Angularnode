bd4ff0da8235ed752050ee5dc6b5be0b569fa2d1
Bridget Sarah <msbskelly@gmail.com>
2019-02-11 18:21:59 +0000

    Moved: Github files into correct directory

--- app.js
+++
@@ -1,8 +0,0 @@
-// Vanilla JS HTTP Server //
-
-var http = require("http");
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("hello world");
-    response.end();
-}).listen(8080);
