2336b57124393c3c22b59fc8ccf175b5035ee900
Bridget Sarah <msbskelly@gmail.com>
2019-02-11 15:34:57 +0000

    Added: NPM Supertest

--- api/controllers/api/posts.js
+++ api/controllers/api/posts.js
@@ -1,44 +1,27 @@
 // api/controllers/api/posts.js
-
-var websockets = require('../../websockets')
+var router = require('express').Router()
 var post = require('../../models/post')
+var websockets = require('../../websockets')
 
 
-router.get('/api/posts', function (req, res, next) {
-    post.find()
+router.get('/', function (req, res, next) {
+    Post.find()
     .sort('-date')
-    .exec(function(err, posts) {
-        if (err) {return next (err) }
-        res.json(posts)
+    .exec(function (err, posts) {
+      if (err) { return next(err) }
+      res.json(posts)
     })
-})
-
-router.post('/api/posts'), function (req, res, next) {
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    })
-}
+  })
 
 // find current users username
 router.post('/', function (req, res, next){
     var post = new Post({body: req.body.body})
     post.username = req.auth.username
     post.save(function (err, post) {
-        if (err) {return next(err)}
+        if (err) {return next(err) }
+        pubsub.publish('new_post', post)
+       websockets.broadcast('new_post', post)
         res.json(201, post)
     })
 })
     module.exports = router
-
-    //web sockets module (/skipped p132 - on going issue with node ws module - needs resolving)
-router.post('/', function(req, res, next){
-    var post = new post({req:body.body})
-    post.username = req.auth.username
-    post.save(function (err, post){
-        if (err) { return next(err) }
-        websockets.broadcast('new_post', post)
-        res.json(201, post)
-    })
-})
-
